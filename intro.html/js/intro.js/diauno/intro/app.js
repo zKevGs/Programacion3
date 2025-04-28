01b856e020a6e907231c34db83fbf12cf99f4987
Kevin Ceriotti <110489369+zKevGs@users.noreply.github.com>
2025-04-28 15:55:42 -0300

    se subió el dia once (javascript)

---
+++ intro.html/js/intro.js/diauno/intro/app.js
@@ -0,0 +1,50 @@
+/* {
+  console.log(nombre);
+}
+console.log(nombre);
+var nombre = "hola"; */
+
+/*  constantes
+const a = 1;
+a = ""; */
+
+/* operaciones logicas */
+
+/* let a = true,
+  b = true;
+
+console.log("not A: ", !a, "not B: ", !b);
+
+console.log("Or a o b", a || b);
+
+console.log("and a y b", a && b); */
+
+/* operaciones aritmeticas */
+
+/* let a = 1,
+  b = 2;
+
+console.log("suma:", a + b);
+console.log("resta:", a - b);
+console.log("multiplicacion:", a * b);
+console.log("division:", a / b);
+console.log("modulo:", a % b);
+console.log("suma:", a + b++); */
+
+/* concatenacion */
+
+/* let a = "3",
+  b = 1;
+console.log(a - b); resta
+console.log(a + b); concatena*/
+/* 
+let a = true,
+  b = 1;
+
+console.log(b && a); */
+
+/* let numbers = [1, 2, "hola", 2, true];
+console.log(numbers); */
+let numbers = [1, 2, 3];
+let numbers2 = [1, 2, 3];
+console.log(numbers2 === numbers);
